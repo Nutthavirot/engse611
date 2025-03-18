8c41a508900daaf7533c80308ff41daa431671df
Nutthavirot <voltvolt.nut@gmail.com>
2025-03-18 09:05:12 +0700

    lab-7

--- lab7/Homepage/script.js
+++ lab7/Homepage/script.js
@@ -4,15 +4,33 @@ img.onclick = function () {
     img.classList.toggle("azoom");
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-
-    let buttons = document.querySelectorAll("table.table-show button");
-
-
-    buttons.forEach((button, index) => {
-        button.addEventListener("click", function () {
-            
-            window.location.href = `../challenge/challenge${index + 1}.html`;
-        });
-    });
-});
+function ex1() {
+    window.open("http://127.0.0.1:5500/lab7/Challenge/Challenge1.html");
+}
+function ex2() {
+    window.open("http://127.0.0.1:5500/lab7/Challenge/Challenge2.html");
+}
+function ex3() {
+    window.open("http://127.0.0.1:5500/lab7/Challenge/Challenge3.html");
+}
+function ex4() {
+    window.open("http://127.0.0.1:5500/lab7/Challenge/Challenge4.html");
+}
+function ex5() {
+    window.open("http://127.0.0.1:5500/lab7/Challenge/Challenge5.html");
+}
+function ex6() {
+    window.open("http://127.0.0.1:5500/lab7/Challenge/Challenge6.html");
+}
+function ex7() {
+    window.open("http://127.0.0.1:5500/lab7/Challenge/Challenge7.html");
+}
+function ex8() {
+    window.open("http://127.0.0.1:5500/lab7/Challenge/Challenge8.html");
+}
+function ex9() {
+    window.open("http://127.0.0.1:5500/lab7/Challenge/Challenge9.html");
+}
+function ex10() {
+    window.open("http://127.0.0.1:5500/lab7/Challenge/Challenge10.html");
+}
